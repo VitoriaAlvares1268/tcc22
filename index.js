b8b21ae9afbf28e16b86dbaff25b74841dfe81cd
Vitória <vihlopes423@gmail.com>
2022-06-10 01:26:04 -0300

    Criação do metodo get e post

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express = require ('express')
 const app = express()
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var path = require('path')
+
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.set("view engine","ejs")
+app.use(express.static(path.join(__dirname,"public")))
+
+
 app.get('/', function(req, res){
-    res.send("Ola Vitoria")
+    res.render("index.ejs",{})
+})
+
+app.get('/usuarios', function(req, res){
+    res.render("usuarios.ejs",{usuarios:[
+        {nome:"Diego", email:"vihalavres@gmail.com"}, 
+        {nome:"Diego", email:"Ana@gmail.com"}
+    ]})
+})
+// O post é para ter resposta. para formularios etc //
+app.post('/add', function(req,res){
+    console.log("Cheguei até aqui")
+})
+app.get('/add', function(req,res){
+       res.render("Adiciona.ejs")
 })
 
 app.listen(3000, function() {
